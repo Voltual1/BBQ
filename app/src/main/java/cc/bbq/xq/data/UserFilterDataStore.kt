21dd66f689b0c71017a3cb2fbebe436ad047a6d3
Voltual1 <18206039173@163.com>
2026-01-03 01:25:01 +0800

    更新

--- app/src/main/java/cc/bbq/xq/data/UserFilterDataStore.kt
+++ app/src/main/java/cc/bbq/xq/data/UserFilterDataStore.kt
@@ -18,6 +18,7 @@ import androidx.datastore.preferences.core.stringSetPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.first
 import org.koin.core.annotation.Single
 
 // DataStore 实例
